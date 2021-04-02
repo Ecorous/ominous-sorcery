18c3809fe219847ea1f14cc2fe1f2f7c35bdf945
exoplant <lennyj2468+github@gmail.com>
2021-04-02 21:59:40 +0100

    Blockstate broken

--- src/main/java/com/expora/ominous/registry/OSBlocks.java
+++ src/main/java/com/expora/ominous/registry/OSBlocks.java
@@ -1,8 +1,11 @@
 package com.expora.ominous.registry;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -18,5 +21,7 @@ public class OSBlocks
     {
         Registry.register(Registry.BLOCK, new Identifier(MODID, "grendelwood_log"), GRENDELWOOD_LOG);
         Registry.register(Registry.BLOCK, new Identifier(MODID, "grendelwood_planks"), GRENDELWOOD_PLANKS);
+        Registry.register(Registry.ITEM, new Identifier(MODID, "grendelwood_planks"), new BlockItem(GRENDELWOOD_PLANKS, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
+        Registry.register(Registry.ITEM, new Identifier(MODID, "grendelwood_log"), new BlockItem(GRENDELWOOD_LOG, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
     }
 }
